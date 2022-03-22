f2033bc6989eb63345c7d37c2c1806482e0a122d
nyk <0xnykcd@googlemail.com>
2022-03-22 14:10:32 +0100

    background update

---
+++ assets/woofers.js
@@ -0,0 +1,13 @@
+export const PlaygroundAttributes = [
+    {
+        id: 1,
+        title: "Background",
+        background: [
+            {
+                id: 2,
+                title: "East Bay Blue",
+                image: "assets/images/woofers/blue.png",
+                color: "",
+                rarity: "40.00%"
+            }
+        ],
